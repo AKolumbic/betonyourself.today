105f7a9fe83f802dd1dce425444d05b4db015a6e
Andrew Kolumbic <andrewkolumbic@gmail.com>
2025-02-07 22:13:53 -0800

    Update next.config.ts to set output to "standalone" for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone", // Required for Vercel deployment
 };
 
-export default nextConfig;
+module.exports = nextConfig;
