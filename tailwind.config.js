4348950701a3c63dffb16e43aa10b10b9dc4fb0a
Andrew Kolumbic <andrewkolumbic@gmail.com>
2025-02-07 20:48:03 -0800

    Add Tailwind CSS configuration and update layout for new design

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    // if you have other folders, include them here
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
